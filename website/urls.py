e6b21d1c1d64ecaf628dcaebe7c71a195284d497
duarte-cambra-a21805799 <duarte.cambra@gmail.com>
2021-05-23 17:24:22 +0100

    l

--- website/urls.py
+++ website/urls.py
@@ -8,9 +8,9 @@ from . import views
 app_name = "website"
 
 urlpatterns = [
-    path('base', views.home_page_view, name='base'),
     path('home', views.home_page_view, name='home'),
-    path('segundo', views.home_page_view, name='segundo'),
-    path('terceiro', views.home_page_view, name='terceiro')
+    path('base', views.base_page_view, name='base'),
+    path('segundo', views.segundo_page_view, name='segundo'),
+    path('terceiro', views.terceiro_page_view, name='terceiro')
 
 ]
