55f2a915d373a929f1317c7cfc68587f8a81fb54
duarte-cambra-a21805799 <duarte.cambra@gmail.com>
2021-06-04 09:57:33 +0100

    test

--- website/urls.py
+++ website/urls.py
@@ -10,5 +10,10 @@ app_name = "website"
 urlpatterns = [
     path('', views.home_page_view, name='home'),
     path('segundo', views.segundo_page_view, name='segundo'),
-    path('terceiro', views.terceiro_page_view, name='terceiro')
+    path('terceiro', views.terceiro_page_view, name='terceiro'),
+    path('layouts', views.layouts_page_view, name='layouts'),
+    path('multimedia', views.multimedia_page_view, name='multimedia'),
+    path('inventario', views.inventario_page_view, name='inventario'),
+    path('tecnicas', views.tecnicas_page_view, name='tecnicas')
+
 ]
--- website/views.py
+++ website/views.py
@@ -16,3 +16,15 @@ def segundo_page_view(request):
 
 def terceiro_page_view(request):
     return render(request, 'website/terceiro.html')
+
+def layouts_page_view(request):
+    return render(request, 'website/layouts.html')
+
+def multimedia_page_view(request):
+    return render(request, 'website/multimedia.html')
+
+def inventario_page_view(request):
+    return render(request, 'website/inventario.html')
+
+def tecnicas_page_view(request):
+    return render(request, 'website/tecnicas.html')
